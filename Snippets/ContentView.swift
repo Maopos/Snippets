63c8967ea74354cc050ea85570ee5b14e443df95
maopos <mauricioposadamurillo@gmail.com>
2023-11-17 16:43:12 -0500

    Initial Commit

---
+++ Snippets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snippets
+//
+//  Created by Mauricio Posada Murillo on 17/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snippets/SnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnippetsApp.swift
+//  Snippets
+//
+//  Created by Mauricio Posada Murillo on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
